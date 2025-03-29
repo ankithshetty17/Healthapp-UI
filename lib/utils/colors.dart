33269dad2f58ef7835d8ee75bddfacfea0a6c26a
Ankith <ankishetty2002@gmal.com>
2025-03-29 14:01:39 +0530

    init

--- lib/utils/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kPrimaryColor = Color(0xff5873d8);
-const Color secondaryBgColor =  Color.fromARGB(255, 240, 238, 238);
-const Color cardBgColor = Color(0xffebf1f7);
-
-const Color profileCardBgColor = Color(0xff5367c3);
